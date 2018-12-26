7007039cb1172672bb5cb31a0f42af33f7cc574f
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-26 16:56:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,403 @@
+<?php
+include "header.php";
+?>
+
+<body>
+<!-- SECTION -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div id="hot-post" class="row hot-post">
+            <div class="col-md-8 hot-post-left">
+                <!-- post -->
+                <div class="post post-thumb">
+                    <a class="post-img" href="marketing-digitalmarketing-impact.php"><img src="./img/hot-post-1.jpg" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="marketing-digitalmarketing-impact.php">Marketing</a>
+                        </div>
+                        <h3 class="post-title title-lg"><a href="marketing-digitalmarketing-impact.php">Impact of Social Media in Marketing</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">SAKSHAM SHARMA</a></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /post -->
+            </div>
+            <div class="col-md-4 hot-post-right">
+                <!-- post -->
+                <div class="post post-thumb">
+                    <a class="post-img" href="technology-tech-artificialintelligence.php"><img src="./img/hot-post-2.jpg" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="technology-tech-artificialintelligence.php">Technology</a>
+                        </div>
+                        <h3 class="post-title"><a href="technology-trending.artificialintelliegence.php">ARTIFICIAL INTELLIGENCE</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Pankaj M. Patidar</a></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /post -->
+
+                <!-- post -->
+                <div class="post post-thumb">
+                    <a class="post-img" href="technology-tech-iot.php"><img src="./img/BLOGS/internet.jpg" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="technology-tech-iot.php">Technology</a>
+
+                        </div>
+                        <h3 class="post-title"><a href="technology-tech-iot.php">Internet Of Things</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Nandhavardhan Reddy</a></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /post -->
+            </div>
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+<!-- /SECTION -->
+
+<!-- SECTION -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-8">
+                <!-- row -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title">
+                            <h2 class="title">Recent posts</h2>
+                        </div>
+                    </div>
+                    <!-- post -->
+                    <div class="col-md-6">
+                        <div class="post">
+                            <a class="post-img" href="technology-tech-evolution.php"><img src="./img/post-1.jpg" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="technology-tech-evolution.php">Technology</a>
+                                </div>
+                                <h3 class="post-title"><a href="technology-tech-evolution.php">Technology changes are Best or worst? </a></h3>
+                                <ul class="post-meta">
+                                    <li><a href="author.html">Avni Bhardwaj</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /post -->
+
+                    <!-- post -->
+                    <div class="col-md-6">
+                        <div class="post">
+                            <a class="post-img" href="business-startup.php"><img src="./img/BLOGS/blog1-fig3.jpg" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                <a href="technology-tech-evolution.php">Startup</a>
+                             
+                                </div>
+                                <h3 class="post-title"><a href="business-startup.php">Youth: The Phenomenal Brain Of Startups</a></h3>
+                                <ul class="post-meta">
+                                    <li><a href="author.html">Avni Bhardwaj</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /post -->
+
+                    <div class="clearfix visible-md visible-lg"></div>
+
+                    <!-- post -->
+                    <div class="col-md-6">
+                        <div class="post">
+                            <a class="post-img" href="research-R&D.php"><img src="./img/BLOGS/R&D1.png" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="research-R&D.php">R&D</a>
+                                </div>
+                                <h3 class="post-title"><a href="research-R&D.php">RESEARCH AND DEVELOPEMNT</a></h3>
+                                <ul class="post-meta">
+                                    <li><a href="author.html">AVNI BHARDWAJ</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /post -->
+
+                    <!-- post -->
+                    <div class="col-md-6">
+                        <div class="post">
+                            <a class="post-img" href="research-scope.php"><img src="./img/BLOGS/internships.png" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="research-scope.php">Research and Development</a>
+                                  </div>
+                                <h3 class="post-title"><a href="research-scope.php">Scope of Internships in India</a></h3>
+                                <ul class="post-meta">
+                                    <li><a href="author.html">Avni Bhardwaj</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /post -->
+                </div>
+                <!-- /row -->
+
+          
+             
+             <?php
+             include "tutorials.php";
+        
+                     ?> 
+        
+            <!--
+                    include "Fashion&Travel.php";
+             include "Health.php";
+           -->
+
+            
+            </div>
+            <div class="col-md-4">
+                <!-- ad widget-->
+                <div class="aside-widget text-center">
+                    <a href="#" style="display: inline-block;margin: auto;">
+                      <!--  <img class="img-responsive" src="./img/ad-3.jpg" alt=""> -->
+                    </a>
+                </div>
+                <!-- /ad widget -->
+
+                <!-- social widget -->
+                <div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Social Media</h2>
+                    </div>
+                    <div class="social-widget">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook">
+                                <i class="fa fa-facebook"></i>
+                                <span><!--21.2K--><br> Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <span><!--10.2K--><br>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="social-google-plus">
+                                    <i class="fa fa-google-plus"></i>
+                                    <span><!--5K--><br>Google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                  <!-- /social widget -->
+
+            <?php
+            include "cate.php";
+            ?>
+
+
+               <?php
+              ?>
+            </div>
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+<!-- /SECTION -->
+
+<!-- SECTION -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <!-- ad -->
+            <div class="col-md-12 section-row text-center">
+                <a href="#" style="display: inline-block;margin: auto;">
+                 <!--   <img class="img-responsive" src="./img/ad-2.jpg" alt=""> -->
+                </a>
+            </div>
+            <!-- /ad -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+<!-- /SECTION -->
+
+
+<!-- SECTION -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-8">
+                <!-- post -->
+                <div class="post post-row">
+                    <a class="post-img" href="technology-tech-typesofai.php"><img src="./img/BLOGS/hot-post-2.jpg" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="technology-tech-typesofai.php">Technology</a>
+                          </div>
+                        <h3 class="post-title"><a href="technology-tech-typesofai.php">Types Of Artificial Intelligence</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Nandhavardha Reddy</a></li>
+                            <li></li>
+                        </ul>
+                        <p></p>
+                    </div>
+                </div>
+                <!-- /post -->
+
+                <!-- post -->
+                <div class="post post-row">
+                    <a class="post-img" href="technology-tech-iot.php"><img src="./img/BLOGS/internet.jpg" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="technology-tech-iot.php">Technology</a>
+                        </div>
+                        <h3 class="post-title"><a href="technology-tech-iot.php">Internet Of Things</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Nandhavardhan Reddy</a></li>
+                            <li></li>
+                        </ul>
+                        <p></p>
+                    </div>
+                </div>
+                <!-- /post -->
+
+                <!-- post -->
+                <div class="post post-row">
+                    <a class="post-img" href="marketing-digitalmarketing.php"><img src="./img/BLOGS/digital.png" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="marketing-digitalmarketing.php">Marketing</a>
+                        </div>
+                        <h3 class="post-title"><a href="marketing-digitalmarketing.php">Digital Marketing through social media</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Saksham Sharma</a></li>
+                            <li></li>
+                        </ul>
+                        <p></p>
+                    </div>
+                </div>
+                <!-- /post -->
+
+                <!-- post -->
+                <div class="post post-row">
+                    <a class="post-img" href="marketing-digitalmarketing-brand.php"><img src="./img/BLOGS/brand2.png" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="marketing-digitalmarketing-brand.php">Social Marketing</a>
+                           </div>
+                        <h3 class="post-title"><a href="marketing-digitalmarketing-brand.php">Increasing Brand Awareness</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Saksham Sharma</a></li>
+                            <li></li>
+                        </ul>
+                        <p></p>
+                    </div>
+                </div>
+                <!-- /post -->
+                <div class="post post-row">
+                    <a class="post-img" href="marketing-socialmedia.php"><img src="./img/BLOGS/media2.jpg" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="marketing-socialmedia.php">Social Marketing</a>
+                           </div>
+                        <h3 class="post-title"><a href="marketing-socialmedia.php">Scope Of Social Media Marketing</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Saksham Sharma</a></li>
+                            <li></li>
+                        </ul>
+                        <p></p>
+                    </div>
+                </div>
+               
+
+                <!-- post -->
+                <div class="post post-row">
+                    <a class="post-img" href="technology-tech-artificialintelligence.php"><img src="./img/BLOGS/post-4.jpg" alt=""></a>
+                    <div class="post-body">
+                        <div class="post-category">
+                            <a href="technology-tech-artificialintelligence.php">Technology</a>
+                           </div>
+                        <h3 class="post-title"><a href="technology-tech-artificialintelligence.php">Artificial Intelligence</a></h3>
+                        <ul class="post-meta">
+                            <li><a href="author.html">Pankaj M Patidar</a></li>
+                            <li></li>
+                        </ul>
+                        <p></p>
+                    </div>
+                </div>
+                <!-- /post -->
+
+             <!--   <div class="section-row loadmore text-center">
+                    <a href="#" class="primary-button">Load More</a>
+                </div> -->
+            </div>
+            
+            <div class="col-md-4">
+                <!-- galery widget -->
+                <div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Instagram</h2>
+                        
+                    </div>
+                    <?php
+                    include "instagram.php";
+                    ?>
+                    <!-- <div class="galery-widget">
+                        <ul>
+                            <li><a href="#"><img src="./img/galery-1.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="./img/galery-2.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="./img/galery-3.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="./img/galery-4.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="./img/galery-5.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="./img/galery-6.jpg" alt=""></a></li>
+                        </ul>
+                    </div> /-->
+                </div>
+                <!-- /galery widget -->
+
+                <!-- Ad widget -->
+                <div class="aside-widget text-center">
+                    <a href="#" style="display: inline-block;margin: auto;">
+                     <!--   <img class="img-responsive" src="./img/ad-1.jpg" alt=""> -->
+                    </a>
+                </div>
+                <!-- /Ad widget -->
+            </div>
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+<!-- /SECTION -->
+</body>
+
+<?php
+include "footer.php";
+?>
