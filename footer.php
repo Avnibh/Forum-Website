6e9d887464471a309674383ed5d2901c16fa9cb4
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-26 16:57:36 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,107 @@
+<!-- FOOTER -->
+<footer id="footer">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="footer-widget">
+                    <div class="footer-logo">
+                        <a href="index.php" class="logo"><img src="./img/icon.png" alt="" width="160px" height="100px"></a>
+                    </div>
+                    <p>Fuerte developers Forum is a Blogging platform to share your thoughts and opinions about The latest buzz.  </p>
+                    <ul class="contact-social">
+                        <li><a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCd5Q9JTqRklfZR9-Dc6UcTQ" class="social-google-plus"><i class="fa fa-youtube"></i></a></li>
+                        <li><a href="https://www.instagram.com/fuerte_developers/" class="social-instagram"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-widget">
+                    <h3 class="footer-title">Categories</h3>
+                    <div class="category-widget">
+                        <ul>
+                        <li><a href="marketing.php"> MARKETING<span></span></a></li>
+                            <li><a href="technology.php">TECHNOLOGY <span></span></a></li>
+                            <li><a href="business.php">BUSINESS<span></span
+                            ></a></li>
+                            <li><a href="category_tutorials.php">VIDEOS<span></span></a></li>
+                            <li><a href="research.php">RESEARCH & DEVELOPMENT<span></span></a></li>
+                            <li><a href="billionaires.php">BILLIONAIRES<span></span></a></li>
+                           
+                           
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-widget">
+                    <h3 class="footer-title">Tags</h3>
+                    <div class="tags-widget">
+                        <ul>
+                            <li><a href="#">Social</a></li>
+                            <li><a href="#">Business</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Tech</a></li>
+                            <li><a href="#">Technology</a></li>
+                            <li><a href="#">Media</a></li>
+                            <li><a href="#">Digital</a></li>
+                            <li><a href="#">MArketing</a></li>
+                            <li><a href="#">Startups</a></li>
+                            <li><a href="#">Youth</a></li>
+                            <li><a href="#">IOT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-widget">
+                    <h3 class="footer-title">Newsletter</h3>
+                    <div class="newsletter-widget">
+                        <form>
+                            <p>For debate email now.</p>
+                            <input class="input" name="newsletter" placeholder="Enter Your Email">
+                            <button class="primary-button">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /row -->
+
+        <!-- row -->
+        <div class="footer-bottom row">
+            <div class="col-md-6 col-md-push-6">
+                <ul class="footer-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="contact.html">Contacts</a></li>
+                    <li><a href="#">Advertise</a></li>
+                    <li><a href="#">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-md-6 col-md-pull-6">
+                <div class="footer-copyright">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made by <a href="http://www.fuertedevelopers.com" target="_blank">FuerteDevelopers</a></a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </div>
+            </div>
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</footer>
+<!-- /FOOTER -->
+
+<!-- jQuery Plugins -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
