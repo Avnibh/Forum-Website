e0f486bb5e1a78cec72e259b76bb4fa7c81cba91
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-31 23:30:47 +0530

    Add files via upload

---
+++ cate.php
@@ -0,0 +1,20 @@
+    <!-- category widget -->
+                <div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Categories</h2>
+                    </div>
+                    <div class="category-widget">
+                        <ul>
+                            <li><a href="technology.php">TECHNOLOGY <span></span></a></li>
+                            <li><a href="business.php">BUSINESS<span></span></a></li>
+                            <li><a href="marketing.php"> MARKETING<span></span></a></li>
+                           
+                            <li><a href="category_tutorials.php">TUTORIALS<span></span></a></li>
+                            <li><a href="research.php">RESEARCH & DEVELOPMENT<span></span></a></li>
+                            <li><a href="billionaires.php">BILLIONAIRES<span></span></a></li>
+                           
+                           
+                        </ul>
+                    </div>
+                </div>
+                <!-- /category widget -->
