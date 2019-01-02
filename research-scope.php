6ad60cbf9199eefcddcc5013a7c94950b889cbd4
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2019-01-03 00:07:38 +0530

    Add files via upload

---
+++ research-scope.php
@@ -0,0 +1,260 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>Scope of internship in India</h3>
+						<p>The contemporary situation of our country’s employment rate has been really improved because of various youth employment schemes and policies by the government. The earlier scenario showcased educated unemployed youth wandering in search of a decent livelihood and job to earn money. The education sector however does remain the same as it was during the past 5 years. But the only difference that has come up is the opportunities that the youth now has to work. This change in the employment status has not arrived due to some sudden drift but a there had been real brain storming about the issue and the need to accelerate the present job conditions for youth. The digital India campaign that was launched by the prime minister of our country aimed to digitalize the nation along with providing virtual jobs to young generation by which they can financially support themselves and thereafter their families. But now having solved one problem another obstacle may be encountered; Sometimes there arises a situation where the hiring company demands work experience from the candidate and having stepped into the cooperate world for the first time the person may not have any work experience. In such cases the opportunity is of course given to the candidate who will prove to be an asset for the company. </p>
+						<figure class="pull-right">
+							<img src="./img/BLOGS/internship.jpg" alt="internships">
+							<figcaption></figcaption>
+						</figure>
+						<p>An internship can be seen as an exceptional way to try out a plan in certain career field beforehand. A student supposedly wants to choose a fast-paced job in journalism after graduation, but having tried an internship, he might discover that journalism is not the business for him, now that’s a really appreciable insight that will help choose the correct career path. In addition, an internship can be used to create a professional network that can assist with letters of recommendation and lead to future employment opportunities. Internships prove to be a golden chance for the students to hone their skills and know how the professional companies work. It is not that anyone and everyone is entitled to work as an intern in a company. There is a series of qualification criterion that needs to be fulfilled before getting into a reputed firm as an intern.   </p>
+                        
+						<p>Only potential students who match the job requirements are hired. Professional skills, communication, time management, technical insight are some much needed skills nowadays. However, many other profound skills are also being judged upon before hiring an intern. Be it online internships which include work from home opportunity or in-office trainings, this concept of internships is evolving at a very fast pace. This is although a great initiative by the company to discover and enhance the potential of youth, assess it and in return provide incentives for their efforts. It is therefore a substantial stride in students’ development as they learn about professionalism, gain experience. Moreover, they are able to manage their expenses by themselves which gives a sigh of satisfaction and content. They acquire responsibility, efficiency and communication skills to name a few. Thus, the field of internship finds a wide scope in the coming generations and aids in to producing self-reliant and confident individuals. Apart from this an internship surely strengthens students cv. It’s obvious that more experience contributes to a better cv. Having an internship experience definitely gives a student a story to tell beyond having spent his college life merely coming, going and doing nothing like most students tend to do. It gives a student edge over the others. So, it is absolutely something to give a shot and boost your resume. </p>
+                        <blockquote class="blockquote">
+							<p>As it is said “Empty mind is a devil’s place”, Giving chances to deserving candidates to put their mind to work, the internships help students utilize their surplus time into something innovative and most importantly they devote their time for something that interests them, so they give in their sincere efforts.  Considering the current trends in internship domain there has been a rapid increase in this employer- employees duo to take over the professional world. Like earlier times, when lesser strata of youngsters were involved in online earning and skill enhancement through internships, that also inspired more individuals to step into this field and hone their skills and ultimately get paid for their work. Money through internships is actually smart money and most students find it the best solution to meet their daily monetary needs. As said for today’s people: Work smart not hard, so internships thus are a great alternative to tedious jobs demanding hard work. And this sector has tremendous scope for students to exploit their time and efforts in the effectively.  </p>
+						</blockquote>
+						<p></p>
+					</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+					<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="#">AVNI BHARDWAJ</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="#">
+									<img class="author-img media-object" src="./img/avni1.jpeg" alt="Avni_fuerte">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>WEB DEVELOPER</p>
+								<ul class="author-social">
+								<li><a href="https://www.facebook.com/avnibh"><i class="fa fa-facebook"></i></a></li>
+<li><a href="https://github.com/Avnibh"><i class="fa fa-github"></i></a></li>
+<li><a href="https://www.linkedin.com/in/avni-bhardwaj-89b03b167/"><i class="fa fa-linkedin"></i></a></li>
+	</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="business-startup.php"><img src="./img/BLOGS/blog1-fig3.jpg" alt=""></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="business-startup.php">Startup</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="business-startup.php">Youth And Startups</a></h3>
+										<ul class="post-meta">
+										<i class="fa fa-user-o" aria-hidden="true"></i>
+
+											<li><a href="#">Avni Bhardwaj</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+				
+
+					
+					<!-- post comments -->
+				<!--	<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+				<!--			<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+					<!--				<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+					<!--			</div>
+					<!--		</div>
+							<!-- /comment -->
+
+							
+					<!--	</div>
+					</div>
+					<!-- /post comments -->
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Social Media</h2>
+                    </div>
+                    <div class="social-widget">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook">
+                                <i class="fa fa-facebook"></i>
+                                <span><!--21.2K--><br> Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <span><!--10.2K--><br>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+							<a href="https://plus.google.com/u/0/117653667624045995795" class="social-google-plus">
+                                       <i class="fa fa-google-plus"></i>
+                                    <span><!--5K--><br>Google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+              	<!-- /social widget -->
+
+					<!-- category widget -->
+					<?php
+					include "cate.php"
+					?>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<?php
+					include "instagram.php"
+					?>
+					
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
