b1f3b37b1335ec7d59d582de919f8c5bd3f453fc
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2019-01-04 22:40:04 +0530

    Add files via upload

---
+++ tutorials.php
@@ -0,0 +1,69 @@
+   <!-- row -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title">
+                            <h2 class="title">Tutorials / MySQL
+                            </h2>
+                        </div>
+                    </div>
+                    <!-- post -->
+                    <div class="col-md-4">
+                        <div class="post post-sm">
+                            <a class="post-img" href="https://www.youtube.com/embed/-_FIgGUzz88"><img src="./img/BLOGS/5.png" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="https://www.youtube.com/embed/-_FIgGUzz88">Installing XAMPP</a>
+                                </div>
+                                <h3 class="post-title title-sm"><a href="blog-post.html"></a></h3>
+                                <ul class="post-meta">
+                                <i class="fa fa-user-o" aria-hidden="true"></i>
+
+                                    <li><a href="#">Pankaj M Patidar</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- /post -->
+                  
+
+                    <!-- post -->
+                    <div class="col-md-4">
+                        <div class="post post-sm">
+                            <a class="post-img" href="https://www.youtube.com/embed/uEHMa4lFyEM"><img src="./img/BLOGS/1.png" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="https://www.youtube.com/embed/uEHMa4lFyEM">Creating Database</a>
+                                </div>
+                                <h3 class="post-title title-sm"><a href="blog-post.html"></a></h3>
+                                <ul class="post-meta">
+                                <i class="fa fa-user-o" aria-hidden="true"></i>
+
+                                    <li><a href="#">Avni Bhardwaj</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /post -->
+
+                    <!-- post -->
+                   <!-- <div class="col-md-4">
+                        <div class="post post-sm">
+                            <a class="post-img" href="blog-post.html"><img src="./img/post-11.jpg" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                    <a href="category.html">Technology</a>
+                                </div>
+                                <h3 class="post-title title-sm"><a href="blog-post.html">What is Technology?</a></h3>
+                                <ul class="post-meta">
+                                    <li><a href="#">Raghav</a></li>
+                                    <li>20 April 2018</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /post -->
+                </div>
+                <!-- /row -->
